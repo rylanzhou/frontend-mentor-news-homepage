4368bbeb860c517baa94afcbc33588b164d867f5
Rylan Zhou <rylanzhou@outlook.com>
2022-10-30 16:40:30 -0500

    feat: add support for mobile devices

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
+import mobile from 'is-mobile';
 import './style.scss';
+
+let menuUl: HTMLElement | null;
+
+window.onload = () => {
+  if (mobile()) {
+    document.querySelector('#app')?.classList.add('mobile');
+
+    document.querySelector('.menu')?.addEventListener('click', openMenu);
+    document.querySelector('.close')?.addEventListener('click', closeMenu);
+
+    menuUl = document.querySelector<HTMLElement>('nav ul');
+    if (menuUl) {
+      menuUl.style.display = 'none';
+    }
+  }
+
+  document.documentElement.style.setProperty('--height', window.innerHeight + 'px');
+};
+
+window.onresize = () => {
+  document.documentElement.style.setProperty('--height', window.innerHeight + 'px');
+};
+
+function openMenu() {
+  document.querySelector('#mask')?.classList.add('active');
+  if (menuUl) {
+    menuUl.style.display = 'block';
+    setTimeout(() => (menuUl ? menuUl.classList.add('active') : ''), 0);
+  }
+}
+
+function closeMenu() {
+  document.querySelector('#mask')?.classList.remove('active');
+  if (menuUl) {
+    menuUl.classList.remove('active');
+    setTimeout(() => (menuUl ? (menuUl.style.display = 'none') : ''), 300);
+  }
+}
